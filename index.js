a39a5177c3be37b23450c72086a30e2580c9ec3b
Penguin1110 <106901954+Penguin1110@users.noreply.github.com>
2024-12-13 14:47:58 +0800

    upd

--- index.js
+++ index.js
@@ -20,22 +20,22 @@ function playSound(key) {
             new Audio("sounds/crash.mp3").play();
             break;
         case "a":
-            new Audio("sounds/kick.mp3").play();
+            new Audio("sounds/kick-bass.mp3").play();
             break;
         case "s":
             new Audio("sounds/snare.mp3").play();
             break;
         case "d":
-            new Audio("sounds/tom1.mp3").play();
+            new Audio("sounds/tom-1.mp3").play();
             break;
         case "j":
-            new Audio("sounds/tom2.mp3").play();
+            new Audio("sounds/tom-2.mp3").play();
             break;
         case "k":
-            new Audio("sounds/tom3.mp3").play();
+            new Audio("sounds/tom-3.mp3").play();
             break;
         case "l":
-            new Audio("sounds/tom4.mp3").play();
+            new Audio("sounds/tom-4.mp3").play();
             break;
         default:
             console.log("Invalid key pressed:", key);
